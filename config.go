f75d722f44fec7f8db5b733ed66e59f9ae61d9c1
Bontor Irvan F Sianipar <vannleonheart@gmail.com>
2024-05-08 15:38:38 +0700

    fix LoadJsonFile, check if file exist

--- config.go
+++ config.go
@@ -6,6 +6,15 @@ import (
 )
 
 func LoadJsonFile(filePath string, output interface{}) (*[]byte, error) {
+	fileStat, err := os.Stat(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	if fileStat.IsDir() {
+		return nil, os.ErrNotExist
+	}
+
 	fileContent, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, err
